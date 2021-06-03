28810867fa052122cff334196d0c60cc52bffefe
Dimitris Doxopoulos <olonir@hotmail.com>
2021-06-03 11:50:52 +0300

    changed tagline

--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Tagline = () => (
     <div className="tagline">
-        <p>Enter the name of the city you want to see the weather in to get started</p>
+        <p>Check the weather in your city and decide what to wear today!</p>
     </div>
 )
 
